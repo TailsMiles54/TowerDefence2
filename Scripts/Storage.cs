80082d379a5d3a7ca68669eb1521c47c922e26dc
TailsMiles54 <36764942+TailsMiles54@users.noreply.github.com>
2020-03-13 10:38:45 +0700

    First load

---
+++ Scripts/Storage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Storage : MonoBehaviour
+{
+    public float score = 0, money = 1000, health = 100;
+    public Text scoreT, moneyT, healthT;
+    public GameObject[] grounded; //массив с префабами башен против наземных
+    public GameObject[] flying; //массив с префабами башен против воздушных
+}
